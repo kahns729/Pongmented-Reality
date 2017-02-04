c5de38056b49ddee4e456d35cb818cf15789f658
Gabe Terrell <gabterrell@gmail.com>
2017-02-04 17:58:06 -0500

    Added Movement Bounding

--- Pongmented Reality/Ball.swift
+++ Pongmented Reality/Ball.swift
@@ -9,7 +9,16 @@
 import UIKit
 import SceneKit
 
+fileprivate struct Bounds {
+    typealias Bound = (lower: Float, upper: Float)
+    let x: Bound = (-3, 3)
+    let y: Bound = (-3, 3)
+    let z: Bound = (-20, -8)
+}
+
 class Ball: SCNNode {
+    private let bounds = Bounds()
+    
     override init(){
         super.init()
         self.geometry = SCNSphere(radius: 0.5)
@@ -19,10 +28,36 @@ class Ball: SCNNode {
         fatalError("init(coder:) has not been implemented")
     }
     
-    var velocity : SCNVector3 = SCNVector3(x: 0.0, y: 0.0, z: 0.1)
+    var velocity : SCNVector3 = SCNVector3(x: 0.05, y: -0.075, z: 0.095)
     // TODO: Bounce, check position
     
     func updatePosition() {
         self.position = self.position + self.velocity
+        
+        if self.position.x < bounds.x.lower {
+            self.position.x = bounds.x.lower
+            self.velocity.x *= -1
+        }
+        if self.position.y < bounds.y.lower {
+            self.position.y = bounds.y.lower
+            self.velocity.y *= -1
+        }
+        if self.position.z < bounds.z.lower {
+            self.position.z = bounds.z.lower
+            self.velocity.z *= -1
+        }
+        
+        if self.position.x > bounds.x.upper {
+            self.position.x = bounds.x.upper
+            self.velocity.x *= -1
+        }
+        if self.position.y > bounds.y.upper {
+            self.position.y = bounds.y.upper
+            self.velocity.y *= -1
+        }
+        if self.position.z > bounds.z.upper {
+            self.position.z = bounds.z.upper
+            self.velocity.z *= -1
+        }
     }
 }
--- Pongmented Reality/UserPaddle.swift
+++ Pongmented Reality/UserPaddle.swift
@@ -10,14 +10,21 @@ import UIKit
 import SceneKit
 
 let ACCEL_TRUNC_THRESHOLD = SCNVector3(x: 0.0001, y: 0.0001, z: 0.0001)
-let ACCEL_TRUNC_THRESHOLD = SCNVector3(x: 0.0, y: 0.0, z: 0.0)
+//let ACCEL_TRUNC_THRESHOLD = SCNVector3(x: 0.0, y: 0.0, z: 0.0)
 let ACCEL_DAMPEN_FACTOR : Float = 0.02
 
 let VELOC_TRUNC_THRESHOLD = SCNVector3(x: 0.005, y: 0.005, z: 0.005)
 let VELOC_DAMPEN_FACTOR : Float = 1
 
+fileprivate struct Bounds {
+    typealias Bound = (lower: Float, upper: Float)
+    let x: Bound = (-3, 3)
+    let y: Bound = (-3, 3)
+}
+
 class UserPaddle: SCNNode {
     var velocity : SCNVector3 = SCNVector3(x: 0, y: 0, z: 0)
+    private let bounds = Bounds()
     
     override init() {
         super.init()
@@ -36,5 +43,23 @@ class UserPaddle: SCNNode {
     
     func updatePosition() {
         self.position = self.position + self.velocity
+        
+        if self.position.x < bounds.x.lower {
+            self.position.x = bounds.x.lower
+            self.velocity.x = 0
+        }
+        if self.position.y < bounds.y.lower {
+            self.position.y = bounds.y.lower
+            self.velocity.y = 0
+        }
+        
+        if self.position.x > bounds.x.upper {
+            self.position.x = bounds.x.upper
+            self.velocity.x = 0
+        }
+        if self.position.y > bounds.y.upper {
+            self.position.y = bounds.y.upper
+            self.velocity.y = 0
+        }
     }
 }
